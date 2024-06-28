66ca0204d8d74659e6fadac95b7cda45425eab44
randrievskiy <ruslan.andriievskyi@vista.com>
2024-06-28 23:37:37 +0300

    HUD. Battery level

--- CourseProject/Tracking.py
+++ CourseProject/Tracking.py
@@ -1,27 +1,34 @@
-import time
-import numpy as np
 import cv2
 from djitellopy import tello
+import HUD
 
 tello = tello.Tello()
 tello.connect()
-print(tello.get_battery())
 
-#w, h = 360, 240
+w, h = 400, 280
 
 tello.streamon()
 
 # Define the codec and create VideoWriter object
 # 'XVID' is a codec for the .avi file format, you can change it to others like 'MJPG'
-#fourcc = cv2.VideoWriter.fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi', fourcc, 30.0, (960, 720))
+# fourcc = cv2.VideoWriter.fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi', fourcc, 30.0, (960, 720))
 
 while True:
     frame = tello.get_frame_read().frame
-    #frame = cv2.resize(frame, (w, h))
-
+    # frame = cv2.resize(frame, (w, h))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     # Write the frame to the video file
-    #out.write(frame)
+    # out.write(frame)
+
+    battery_level = tello.get_battery()
+
+    position = (frame.shape[1] - 110, 15)  # Adjust the position as needed
+    HUD.draw_battery_icon(frame, battery_level, position)
+
+    height, width, channels = frame.shape
+
+    HUD.draw_battery_percentage(frame, battery_level, width)
 
     cv2.imshow('Video', frame)
     cv2.waitKey(1)
